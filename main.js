1e8067b15634e6819d73cbce00457ec953d1945d
Sondre <sondre_berdal@hotmail.com>
2017-02-13 11:55:57 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Brett from './Brett.js'
+
+
+
+
+
+
+ReactDOM.render(
+	<Brett/>,
+	document.getElementById('root')
+);
+
+
+class box extends React.Component {
+	render(){
+		<div>en fin boks</div>
+	}
+
+}
